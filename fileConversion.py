665e70074de35fd33300b0be632f2df1a1ee784c
AvinashSubhash <avinashsubhash19@gmail.com>
2023-03-08 01:09:52 +0530

    completed-working-backend-for-the-app

--- fileConversion.py
+++ fileConversion.py
@@ -1,30 +1,26 @@
-def fileNameToBinary(fileName):
-    binary = ''.join(format(ord(char),'08b') for char in fileName)
-    return binary
-    #return [binary,binaryToFilename(binary)]
-
-def binaryToFilename(binary):
-    bytes_list = [int(binary[i:i+8], 2) for i in range(0, len(binary), 8)]
-    string = ''.join([chr(byte) for byte in bytes_list])
-    return string
+INITIAL_DATA = 0
+def binaryToFile(filename,binary_string):
+    byte_array = bytearray()
+    for i in range(0, len(binary_string), 8):
+        byte = int(binary_string[i:i+8], 2)
+        byte_array.append(byte)
+    LATTE = byte_array
+    #print(byte_array)
+    with open(filename,'wb') as file:
+        file.write(byte_array)
 
 def fileToBinary(filename):
     with open(str(filename),'rb') as f:
         contents = f.read()
         binary = ''.join(format(byte,'08b') for byte in contents)
-        #binaryToFilename("result.txt",binary)   
-        #print("File to binary: ",binary) 
+        INITIAL_DATA = binary
+        #binaryToFile("final_output.pdf",binary)   
+        
     return binary
 
-def binaryToFilename(filename,binary_string):
-        bytes_list = [int(binary_string[i:i+8], 2) for i in range(0, len(binary_string), 8)]
-        binary = bytes(bytes_list)
-        with open(filename,'wb') as file:
-            file.write(binary)
-
 import videoConversion as vc
-
-vc.binaryToVideo(fileToBinary("solidity.pdf"))
-print("Video to binary: ",vc.videoToBinary("binary_video.mp4"))
-
+vc.binaryToVideo(fileToBinary("Codechef-problems.zip"))
+#print("Video to binary: ",vc.videoToBinary("binary_video.mp4"))
+binary = vc.videoToBinary("Codechef-problems.mp4")
+#_ = binaryToFile("out.zip",binary)
 
--- videoConversion.py
+++ videoConversion.py
@@ -1,21 +1,19 @@
 import cv2
 import numpy as np
-
+INITIAL=6123128
 def binaryToVideo(binary_data):
     array = np.array(list(binary_data)).astype(np.uint8)
+    res_array = array
     size = len(binary_data)
+    print("Initial size :",size)
     frame_size = (128,128)
     frames = int(np.ceil(size/(frame_size[0]*frame_size[1])))
     difference = int(frames*128*128 - size)
+    print("Difference: ",difference)
     additional = np.zeros(difference,)
     array = np.append(array,additional)
-    print(frames)
-    while frames%3!=0:
-        frames+=1
-        array = array.append(array,np.zeros(frame_size[0]*frame_size[1]))
-    print(frames)
-    array = array.reshape(frames,frame_size[0],frame_size[1])*255
-    print(array.shape)
+    array = array.reshape(frames,frame_size[0],frame_size[1])
+    print("Array shape: ",array.shape)
     array = array.astype(np.uint8)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     fps = 30
@@ -24,21 +22,29 @@ def binaryToVideo(binary_data):
     
     out = cv2.VideoWriter('output.mp4',fourcc,fps,(width,height))
 
-    for i in range(0,frames,3):
-        image = cv2.merge((array[i,:,:],array[i+1,:,:],array[i+2,:,:]))
-        out.write(image)
+    for i in range(frames):
+        img = np.zeros((frame_size[0], frame_size[1], 3), dtype=np.uint8)
+        img[array[i] == 1] = [255, 255, 255]
+        out.write(img)
     out.release()
 
 
 def videoToBinary(video_path):
-    binary_data = bytearray()
+    binary_data = []
     cap = cv2.VideoCapture("output.mp4")
     while True:
         ret,frame = cap.read()
         #print(ret,frame)
         if not ret:
             break
-        print((frame[:,:,0]/255).astype(int))
-        #binary_data.extend([frame[0,0,0],[frame[0,0,1],[frame[0,0,2]])
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        ret,frame = cv2.threshold(frame, 127, 1, cv2.THRESH_BINARY)
+        data = frame.astype(np.uint8).tolist()
+        for i in data:
+            binary_data.extend(i)
+    print("Length of binary data recovered: ",len(binary_data))
+    binary_data = binary_data[:INITIAL]
+    #print(array[:10],binary_data[:10])
+    res = ''.join(str(i) for i in binary_data)
     cap.release()
-    return binary_data
+    return res
